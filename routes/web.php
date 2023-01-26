09817f100021d2efce2ed14ff7a04ae1aad9243b
jose <joseantbonmun@gmail.com>
2023-01-26 19:58:42 +0000

    A32.Ejercicio 1. Primeras rutas

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('community', [App\Http\Controllers\CommunityLinkController::class, 'index']);
+Route::post('community', [App\Http\Controllers\CommunityLinkController::class, 'store']);
+
 require __DIR__.'/auth.php';
