bb83d8069c981b8955eff72b89d802cfe74ae32a
jose <joseantbonmun@gmail.com>
2023-01-31 02:50:39 +0000

    A33.ejer1.Modifico rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,33 @@ Route::middleware('auth')->group(function () {
 
 Route::get('community', [App\Http\Controllers\CommunityLinkController::class, 'index']);
 Route::post('community', [App\Http\Controllers\CommunityLinkController::class, 'store']);
+/* Ejercicio A33. Mas sobre rutas
+Route::get('/user/{name?}', function ($name = null) {
+    return $name;
+});
+*/
+Route::get('/user/{name?}', function ($name = 'Invitado') {
+    return "Bienvenido a la página de pruebas {$name}";
+});
+
+Route::post('/paginaPruebaPost', function ($name = 'Pruebas') {
+    return "Bienvenido a la página de pruebas POST";
+});
+
+Route::match(['get', 'post'], '/2metodos', function () {
+    return "pagina de pruebas que permite GET y POST";
+});
+
+Route::get('/compruebaNombre/{name}', function () {
+    return "página a la que puedes acceder al comprobar que el nombre del parámetro está formado solo con letras";
+})->where('name', '[A-Za-z]+');
+
+Route::get('/compruebaNumero/{id}', function () {
+    return "página a la que puedes acceder al comprobar que el parámetro está formado solo con números";
+})->where('id', '[0-9]+');
+
+Route::get('/compruebaNumeroNombre/{id}/{name}', function ($id, $name) {
+    return "página a la que puedes acceder al comprobar que el 1er.parámetro está formado solo con números y el 2º parámetro solo con letras";
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
 
 require __DIR__.'/auth.php';
