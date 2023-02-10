9a8ebb5981b7e8bda6f6bf1d4751427fe960e0a0
jose <joseantbonmun@gmail.com>
2023-02-10 14:22:27 +0000

    A35.Creo modelo y controlador  Channel en artisan

---
+++ app/Models/Channel.php
@@ -0,0 +1,13 @@
+<?php
+/* Creo el modelo y controlador de Channel en artisan con:
+* php artisan make:controller ChannelController --model=Channel
+*/
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Channel extends Model
+{
+    use HasFactory;
+}
