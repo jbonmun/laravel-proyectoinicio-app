d9f5ff22d39ad97694cc5b75673df77c528e2f06
jose <joseantbonmun@gmail.com>
2023-02-10 12:54:45 +0000

    A34.Ej6.Agree Partial add-linkperose poneAbajo

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.app')
-
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <h1>Community</h1>
+            
+            @foreach ($links as $link)
+            <li>
+                <a href="{{$link->link}}" target="_blank">
+                    {{$link->title}}
+                </a>
+                <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
+            </li>
+            @endforeach
+
+        </div>
+
+    </div>
+    {{$links->links()}}
+
+    
+
+</div>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
 @include('layouts.add-link')
 
 @stop
